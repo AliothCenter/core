09bf8b8e60b1822359dc3982f4e60330d5b7b75e
sunist-c <sunist@mail.sunist.cn>
2023-09-18 20:39:57 +0800

    [dev/options] 更新golang版本，使用1.21.1，更新部分变量命名规则，更新主程序入口

--- go.mod
+++ go.mod
@@ -1,22 +1,26 @@
 module studio.sunist.work/platform/alioth-center
 
-go 1.21.0
+go 1.21.1
 
 require (
 	github.com/gin-gonic/gin v1.9.1
+	github.com/go-redis/redis/v8 v8.11.5
 	github.com/google/uuid v1.3.0
 	github.com/sirupsen/logrus v1.9.3
+	golang.org/x/crypto v0.12.0
 	google.golang.org/grpc v1.57.0
 	google.golang.org/protobuf v1.31.0
-	gopkg.in/yaml.v3 v3.0.1
+	gopkg.in/yaml.v2 v2.4.0
 	gorm.io/driver/postgres v1.5.2
 	gorm.io/gorm v1.25.4
 )
 
 require (
 	github.com/bytedance/sonic v1.10.0 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
 	github.com/chenzhuoyu/iasm v0.9.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
@@ -41,9 +45,9 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	golang.org/x/arch v0.4.0 // indirect
-	golang.org/x/crypto v0.12.0 // indirect
 	golang.org/x/net v0.14.0 // indirect
 	golang.org/x/sys v0.11.0 // indirect
 	golang.org/x/text v0.12.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,39 +2,46 @@ package main
 
 import (
 	"fmt"
-	"net"
-
 	"github.com/gin-gonic/gin"
 	"google.golang.org/grpc"
-
+	"net"
 	"studio.sunist.work/platform/alioth-center/core/restoration"
-	log "studio.sunist.work/platform/alioth-center/infrastructure/global/utils/logger"
+	"studio.sunist.work/platform/alioth-center/core/stellar"
+	"studio.sunist.work/platform/alioth-center/infrastructure/database"
 	"studio.sunist.work/platform/alioth-center/infrastructure/initialize"
+	log "studio.sunist.work/platform/alioth-center/infrastructure/utils/logger"
 )
 
 func main() {
+	// 初始化数据库
+	database.SyncDatabase()
+
+	// 初始化rpc和http服务器
 	logger := log.DefaultLogger()
 	lis, err := net.Listen("tcp",
 		fmt.Sprintf("%s:%d", initialize.GlobalConfig().Grpc.ListenIP, initialize.GlobalConfig().Grpc.ListenPort))
 	if err != nil {
 		panic(err)
 	}
-
-	// 初始化rpc和http服务器
 	s := grpc.NewServer()
-	engine := gin.New()
+	engine := gin.Default()
 	engine.Use(gin.Recovery())
 	external := engine.Group("/external")
 
 	// 注册rpc和http服务器
 	restoration.InitRestorationRpcServer(s)
 	restoration.InitRestorationHttpServer(external)
+	stellar.InitStellarRpcServer(s)
+	stellar.InitStellarHttpServer(external)
 
 	// 启动rpc和http服务器
 	rpcExit := startRpcEngine(s, lis)
 	httpExit := startHttpEngine(engine,
 		fmt.Sprintf("%s:%d", initialize.GlobalConfig().Http.ListenIP, initialize.GlobalConfig().Http.ListenPort))
 	logger.Log(log.DefaultField().WithCaller(log.Internal).WithLevel(log.Info).WithMessage("server(s) started"))
+
+	// 注册服务到stellar
+	restoration.RegisterToStellar()
 
 	// 等待rpc和http服务器退出
 	select {
